e66a9664eb6ce1da4fb557a8900dfeca0c9bb280
chanmi94 <cksal4897@gmail.com>
2022-03-05 22:36:05 +0900

    componets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+// import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edi <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+// function App() {
+//   return <div className="App">hello react dd</div>;
+// }
+
+// export default App;
+
+import React, { Component } from "react";
+import "./App.css";
+
+import TOC from "./components/TOC";
+
+class Subject extends Component {
+  render() {
+    return (
+      <header>
+        <h1>{this.props.title}</h1>
+        {this.props.sub}
       </header>
-    </div>
-  );
+    );
+  }
+}
+
+class App extends Component {
+  //먼저 초기화 시켜주고싶은 함수
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: { title: "web", sub: "world" },
+      contents: [
+        { id: 1, title: "HTML", desc: "html sdfdd" },
+        { id: 2, title: "CSS", desc: "css sdfdd" },
+        { id: 3, title: "JAVA", desc: "java sdfdd" },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+        ></Subject>
+        <Subject title="web" sub="react"></Subject>
+        <TOC data={this.state.contents}></TOC>
+      </div>
+    );
+  }
 }
 
 export default App;
